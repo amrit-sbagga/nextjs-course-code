ea794d32112eca8cf25a7565d851c4a5f721c274
Amrit Pal Singh <amrit.s@proximity.tech>
2022-01-09 19:34:34 +0530

    Navigating programmatically using next router

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -4,10 +4,21 @@ import { useRouter } from 'next/router';
 export default function ClientsProjectPage() {
     const router = useRouter();
     console.log(router.query);
+
+    function loadProjectHandler(){
+        //load data...
+        //router.replace('/clients/monty/projA')
+        //router.push('/clients/monty/projA')
+        router.push({
+            pathname : '/clients/[id]/[clientprojectid]',
+            query : {id : router.query.id, clientprojectid : 'ProjectA'}
+        })
+    }
     
     return (
         <div>
             <h1>Projects of Given Client</h1>
+            <button onClick={loadProjectHandler}>Load Project A</button>
         </div>
     )
 }
