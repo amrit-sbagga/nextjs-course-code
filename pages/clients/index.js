66830404682e0ad4af2f6e2aadb2e4db4b356eda
Amrit Pal Singh <amrit.s@proximity.tech>
2022-01-09 19:22:42 +0530

    Navigating to dynamic routes

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
 
 export default function ClientsPage() {
+    const clients = [
+        { id : "monty", name : "Monty" },
+        { id : "honey", name : "Honey" },
+        { id : "jk", name : "JK" },
+        { id : "ns", name : "NP" }
+    ]
     return (
         <div>
             <h1>Clients Page</h1>
+            <ul>
+                {
+                    clients.map((client) => {
+                        return (<li key={client.id}>
+                            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+                        </li>)
+                    })
+                }
+            </ul>
         </div>
     )
 }
