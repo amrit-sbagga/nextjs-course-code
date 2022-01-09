0fd0976fc77b97986e7247c37a056de32e3fc3f8
Amrit Pal Singh <amrit.s@proximity.tech>
2022-01-09 18:47:03 +0530

    Working with dynamic path segment data value

--- pages/portfolio/[projectid].js
+++ pages/portfolio/[projectid].js
@@ -1,6 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { useRouter } from 'next/router';
 
 export default function PortfolioProjectPage() {
+
+    const router = useRouter();
+    console.log(router.pathname);
+    console.log(router.query);
+
     return (
         <div>
             <h1>Portfolio Project Page</h1>
