581ca72b21328177ec9c7be2cc3b80e3e3801152
Amrit Pal Singh <amrit.s@proximity.tech>
2022-01-09 19:12:07 +0530

    Navigating with Link component

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import Link from 'next/link';
 
 export default function HomePage() {
   return (
     <div>
       <h1>Hello Nextjs</h1>
+      <ul>
+        <li>
+          <Link href="/portfolio">Portfolio</Link>
+        </li>
+        <li>
+          <Link href="/clients">Clients</Link>
+        </li>
+      </ul>
     </div>
   )
 }
